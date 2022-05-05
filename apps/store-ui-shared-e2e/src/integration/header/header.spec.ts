0bdad2ba35c341c719897e323354941b6c907d1e
Suwigya Rathore <suwigya@kramphub.com>
2022-05-05 11:27:19 +0300

    done with e2e

---
+++ apps/store-ui-shared-e2e/src/integration/header/header.spec.ts
@@ -0,0 +1,8 @@
+describe('store-ui-shared: Header component', () => {
+  beforeEach(() => cy.visit(
+    '/iframe.html?id=header--primary&args=title:BoardGameHoard'
+  ));
+    it('should show the title', () => {
+      cy.get('header').contains('BoardGameHoard');
+    });
+});
