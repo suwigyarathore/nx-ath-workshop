3033cab128438ec54a9668bb36f5009f3c7f897c
Suwigya Rathore <suwigya@kramphub.com>
2022-05-06 11:33:30 +0300

    commit comp lib

---
+++ tools/generators/feature-component-lib/index.ts
@@ -0,0 +1,15 @@
+import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
+import { componentGenerator, } from '@nrwl/react';
+
+export default async function (tree: Tree, schema: any) {
+  await componentGenerator(tree, {
+    ...schema,
+    name: 'smart'+ schema.name,
+    style:'scss',
+    project: 'store'
+  })
+  await formatFiles(tree);
+  return () => {
+    installPackagesTask(tree);
+  };
+}
